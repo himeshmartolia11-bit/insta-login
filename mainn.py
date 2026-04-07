c5f578fc91e98780ac16b899bb75a0d6d701376f
himeshmartolia11-bit <himeshmartolia11@gmail.com>
2026-04-07 11:33:32 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,26 +1,21 @@
 import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import json
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 try:
-    # Secrets se load karna
-    raw_key = st.secrets["my_key"]
-    key_dict = json.loads(raw_key, strict=False) # strict=False se hidden characters ignore ho jate hain
-    
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(key_dict, scope)
+    # Direct file read
+    creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open_by_key("1Wu7gvmumWYikaTBGQO41tXeNX8Xh2Sa0lc-Sucb3N20").sheet1
-
-    st.title("Registration Form")
-    username = st.text_input("Enter username")
-    password = st.text_input("Enter password", type="password")
+    
+    st.title("Form")
+    u = st.text_input("Enter username")
+    p = st.text_input("Enter password", type="password")
     
     if st.button("Submit"):
-        sheet.append_row([username, password])
-        st.success("Success! ✅")
-        
+        sheet.append_row([u, p])
+        st.success("Data saved! ✅")
 except Exception as e:
     st.error(f"Error: {e}")
