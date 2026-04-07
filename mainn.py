25be3801748c16e661e28a11a41633bcaa7cdf12
himeshmartolia11-bit <himeshmartolia11@gmail.com>
2026-04-07 11:17:05 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -6,13 +6,13 @@ import json
 # Scope define karna
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-# Secrets se key load karna
 try:
+    # Secrets se key load karna
     key_dict = json.loads(st.secrets["my_key"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(key_dict, scope)
     client = gspread.authorize(creds)
     
-    # Sheet open karna (ID method)
+    # Sheet open karna
     sheet = client.open_by_key("1Wu7gvmumWYikaTBGQO41tXeNX8Xh2Sa0lc-Sucb3N20").sheet1
     
     st.title("Registration Form")
@@ -25,3 +25,6 @@ try:
     if st.button("Submit"):
         sheet.append_row([username, password, followers, age])
         st.success("Data saved successfully! ✅")
+
+except Exception as e:
+    st.error(f"Error: {e}")
