b573ac35bf970cc2fdbd35145e9505ddc0d4fe6a
himeshmartolia11-bit <himeshmartolia11@gmail.com>
2026-04-07 12:10:31 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -2,23 +2,23 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Google Sheets setup
+# Setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 try:
-    # Direct key.json file se credentials lena
-    creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
+    # Streamlit Secrets se credentials uthana
+    creds_dict = dict(st.secrets["gcp_service_account"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     
-    # Aapki Sheet open karna
     sheet = client.open_by_key("1Wu7gvmumWYikaTBGQO41tXeNX8Xh2Sa0lc-Sucb3N20").sheet1
     
-    st.title("Form")
-    u = st.text_input("Enter username")
-    p = st.text_input("Enter password", type="password")
+    st.title("Registration Form")
+    u = st.text_input("Username")
+    p = st.text_input("Password", type="password")
     
     if st.button("Submit"):
         sheet.append_row([u, p])
-        st.success("Saved successfully! ✅")
+        st.success("Bhej diya! ✅")
 except Exception as e:
     st.error(f"Error: {e}")
